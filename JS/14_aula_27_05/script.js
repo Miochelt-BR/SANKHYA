67c38f2a48ac2b99611b96e483ce42b1e2813656
Miochelt1@ <thiago.m.a@outlook.com>
2024-05-27 11:26:13 -0300

    Primeira aula1

---
+++ JS/14_aula_27_05/script.js
@@ -0,0 +1,80 @@
+// aula de variaveis (let, var, const) 
+
+// let nome;
+// const idade;
+// var sexo;
+// const numero=10;
+
+// quando uso o var 
+// hoisting (hoisted)leva para top do escopo pode ser utilizada antes da criação 
+// redeclaração 
+
+
+// exemplo
+//  nome= "thiago";
+// console.log(nome);
+// nome="thiago Michhek"
+// console.log(nome);
+//  idade =33;
+//  console.log(idade);
+//  idade=34;
+//  console.log(idade);
+
+// momentos
+// let idade =25;//number
+// let nome = "joao";//string
+// let ativo = true;//boolean
+// let indefinido=indefined;//indefinido
+// let lista = [1 ,2 ,3 ,4 ,];//array
+// let pessoa = {nome:"carlos",idade:20}
+
+// // OPERADORES ARITMÉTICOS
+// let a = 5 + 3; // Resultado: 8
+// let b = "Ola" + "Mundo"; // Resultado: "OlaMundo"
+// let c = 10 - 5; // Resultado: 5
+// let d = 2 * 3; // Resultado: 6
+// let e = 10 / 2; // Resultado: 5
+// let f = 10 % 3; // Resultado: 1 (resto da divisao de 10 por 3 eh 1)
+
+// OPERADORES DE INCREMENTO E DECREMENTO
+/*
+    Pré-incremento (++x)
+    Pré-decremento (--x)
+*/
+// let k = 5; //5
+// console.log(k)
+// let z = ++k; //6
+// let h = 1 + k; //6
+// console.log(z);
+
+/*
+    Pós-incremento (x++)
+    Pós-decremento (x--)
+*/
+
+// // let k = 5; 
+// // let z = k--; 
+// console.log(z);
+// console.log(k);
+
+// // OPERADORES DE ATRIBUIÇÃO (=)
+// // Atribuição com operação adicional (+=)
+// //Atribuição com operação de subtração (-=)
+// // Atribuição com operação de multiplicação(*=)
+// // Atribuição com operação de divisão (/=)
+// let valor = 5;
+// valor *= 3; // 15
+
+// // OPERADORES DE COMPARAÇÃO
+// // Igual (==)
+// // Igual estrito (===)
+// // Diferente (!=)
+// // Diferente estrito (!==)
+// let valor1 = 10;
+// let valor2 = "10";
+// console.log(valor1 != valor2);
+
+// // Maior que (>), Maior ou igual a (>=), Menor que (<), Menor ou igual a (<=)
+
+// // OPERADORES LÓGICOS
+// // AND lógico (&&)
