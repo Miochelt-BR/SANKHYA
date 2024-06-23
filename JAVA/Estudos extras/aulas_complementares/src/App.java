741cbc0b8349ddc7af6e7620b4a7c46d0f1c9716
Miochelt1@ <thiago.m.a@outlook.com>
2024-06-23 02:42:00 -0300

    aulas complementares

---
+++ JAVA/Estudos extras/aulas_complementares/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+    //    condicpes simples
+        // double saldo = 25.0;
+        // double valorSolicitado=17.0;
+        //  if (valorSolicitado <saldo) {
+        //     saldo = saldo - valorSolicitado;
+        //     System.out.println(saldo);  
+        //  }
+//    condicoes vompostas 
+         
+    }
+}
