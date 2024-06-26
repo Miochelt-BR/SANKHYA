086b8c64c0c039a7bbcbeac6cbb7fe6d42ccdc9a
Miochelt1@ <thiago.m.a@outlook.com>
2024-06-26 14:13:42 -0300

    aula 3 exercicios

--- JAVA/03_aula_26_06/AtividadesAula3/src/main/java/org/example/Ordenacao.java
+++ JAVA/03_aula_26_06/AtividadesAula3/src/main/java/org/example/Ordenacao.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Ordenacao {
     public static void main(String[] args) {
         //Implemente um algoritmo que ordene um array de objetos com base em um critério personalizado (por exemplo, ordem alfabética de strings ou ordenação numérica).
-        int [] a= { 1, 2, 3, 4, 5};
-        for(int i = 0; i < a.length; i++){
-            System.out.println(a[i]);
+        String[] names = {"Douglas", "Dennys", "Luciana", "Alice", "Bob"};
+
+        // Chama a função para ordenar o array de nomes
+        Arrays.sort(names);
+
+
+        System.out.println("Sorted array of strings: " + Arrays.toString(names));
 
     }
 
