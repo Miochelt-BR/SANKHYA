a46b96cb5943ea323834e2dc9d79ef68f0ed79de
Miochelt1@ <thiago.m.a@outlook.com>
2024-06-26 20:56:48 -0300

    exercicios atualizados

--- JAVA/03_aula_26_06/AtividadesAula3/src/main/java/org/example/MaiorSeguencia.java
+++ JAVA/03_aula_26_06/AtividadesAula3/src/main/java/org/example/MaiorSeguencia.java
@@ -3,15 +3,33 @@ package org.example;
 public class MaiorSeguencia {
     public static void main(String[] args) {
         //. Maior Sequência Crescente: Encontre a maior sequência crescente de números em um array e imprima os números dessa sequência.
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int maior = array[0];
+        int[] array = {1, 2, 4, 3, 5, 6, 7, 8, 9, 10};
+
+        int maxLength = 1, currentLength = 1;
+        int startIndex = 0, tempStart = 0;
+
         for (int i = 1; i < array.length; i++) {
-            if (array[i] > maior) {
-                maior = array[i];
+            if (array[i] > array[i - 1]) {
+                currentLength++;
+            } else {
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                    startIndex = tempStart;
+                }
+                currentLength = 1;
+                tempStart = i;
             }
         }
-        System.out.println( "numero maior "+ maior);
 
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+            startIndex = tempStart;
+        }
+
+        System.out.println("Maior sequência crescente: ");
+        for (int i = startIndex; i < startIndex + maxLength; i++) {
+            System.out.print(array[i] + " ");
+        }
 
     }
 }
